6b6908a5c713a0b536278b9647bbec8970b68b05
Darren Britton <darrenbritton07@gmail.com>
2021-12-11 02:49:39 +0000

    feat: basic swagger

--- src/index.js
+++ src/index.js
@@ -7,18 +7,36 @@ import swaggerDocument from '../swagger.json'
 
 import { getUnifiDeviceData } from './unifi.js';
 
+
+// simple logger for this router's requests
+// all requests to this router will first hit this middleware
+
 const app = express();
+const router = express.Router();
 
 const port = process.env.PORT || 3000;
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api', router);
 
-app.get('/', async (req, res) => {
+
+router.use(function(req, res, next) {
+  console.log(`${req.method}: ${req.url}/${req.path}`);
+  next();
+});
+
+router.get('/', async (req, res) => {
   const data = await getUnifiDeviceData();
   res.json(data);
 });
 
-app.get('/device/:deviceId', async (req, res) => {
+router.get('/device/ids', async ( req, res) => {
+  const { data } = await getUnifiDeviceData();
+  const ids = data.map(({ _id, name }) => ({ [_id]: name}));
+  res.json({ ids });
+});
+
+router.get('/device/:deviceId', async (req, res) => {
   const { data } = await getUnifiDeviceData();
   const device = data.find(device => device._id === req.params.deviceId);
   res.json(device);
